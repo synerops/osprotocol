f3c5e9551130afa3421cbe13074c3e15d1da2631
Ronny Badilla <info@ronnybadilla.com>
2026-01-06 10:14:11 -0600

    feat: layouts

--- docs/app/layout.tsx
+++ docs/app/layout.tsx
@@ -1,14 +1,23 @@
-import { RootProvider } from 'fumadocs-ui/provider/next';
 import './global.css';
-import { Inter } from 'next/font/google';
+import { RootProvider } from 'fumadocs-ui/provider/next';
+import { Inter, JetBrains_Mono } from 'next/font/google';
 
 const inter = Inter({
   subsets: ['latin'],
+  variable: '--font-inter',
+});
+
+const jetbrainsMono = JetBrains_Mono({
+  subsets: ['latin'],
+  variable: '--font-jetbrains-mono',
 });
 
 export default function Layout({ children }: LayoutProps<'/'>) {
   return (
-    <html lang="en" className={inter.className} suppressHydrationWarning>
+    <html
+      lang="en"
+      className={`${inter.className} ${jetbrainsMono.variable}`} suppressHydrationWarning
+    >
       <body className="flex flex-col min-h-screen">
         <RootProvider>{children}</RootProvider>
       </body>
--- docs/lib/layout.shared.tsx
+++ docs/lib/layout.shared.tsx
@@ -1,9 +1,31 @@
 import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
+import Image from "next/image";
+
+import LogoDark from "public/logo-dark.svg";
+import LogoLight from "public/logo-light.svg";
+
+export const logo = (
+  <div className="flex items-center">
+    <Image
+      alt="OSP logo"
+      src={LogoDark}
+      className="hidden dark:block w-8"
+      aria-label="OSP logo"
+    />
+    <Image
+      alt="OSP logo"
+      src={LogoLight}
+      className="block dark:hidden w-8"
+      aria-label="OSP logo"
+    />
+    <div className="font-mono pl-2">os://</div>
+  </div>
+);
 
 export function baseOptions(): BaseLayoutProps {
   return {
     nav: {
-      title: 'os://',
+      title: <>{logo}</>,
     },
   };
 }
