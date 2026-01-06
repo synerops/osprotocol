7ba1c71c9bf718a99d7560300e6ab26f18d93ddd
Ronny Badilla <info@ronnybadilla.com>
2026-01-06 15:51:33 -0600

    fixes

--- website/app/layout.tsx
+++ website/app/layout.tsx
@@ -1,8 +1,8 @@
 import './global.css';
 import { RootProvider } from 'fumadocs-ui/provider/next';
-import { 
-  Work_Sans, 
-  JetBrains_Mono, 
+import {
+  Work_Sans,
+  JetBrains_Mono,
   Plus_Jakarta_Sans,
 } from 'next/font/google';
 
@@ -25,7 +25,8 @@ export default function Layout({ children }: LayoutProps<'/'>) {
   return (
     <html
       lang="en"
-      className={`${workSans.variable} ${plusJakartaSans.variable} ${jetbrainsMono.variable}`} suppressHydrationWarning
+      className={`${workSans.variable} ${plusJakartaSans.variable} ${jetbrainsMono.variable}`}
+      suppressHydrationWarning
     >
       <body className="flex flex-col min-h-screen font-sans">
         <RootProvider>{children}</RootProvider>
