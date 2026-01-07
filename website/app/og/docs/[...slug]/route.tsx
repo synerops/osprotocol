5f8a595fd5cb96d54407dcf9372090a344cb17e2
Ronny Badilla <info@ronnybadilla.com>
2026-01-06 18:13:05 -0600

    feat: og

--- website/app/og/docs/[...slug]/route.tsx
+++ website/app/og/docs/[...slug]/route.tsx
@@ -1,24 +1,59 @@
-import { getPageImage, source } from '@/lib/source';
+import { readFile } from "node:fs/promises";
+import { join } from "node:path";
+
 import { notFound } from 'next/navigation';
 import { ImageResponse } from 'next/og';
-import { generate as DefaultImage } from 'fumadocs-ui/og';
+import { getPageImage, source } from '@/lib/source';
+import { OGImage } from '@/components/og';
 
 export const revalidate = false;
 
+async function loadFonts() {
+  const [ jetbrainsMono, workSans ] = await Promise.all([
+    readFile(
+      join(process.cwd(), 'public', 'fonts', 'JetBrainsMono-Regular.ttf')
+    ).then((buffer) => buffer.buffer),
+
+    readFile(
+      join(process.cwd(), 'public', 'fonts', 'WorkSans-Regular.ttf')
+    ).then((buffer) => buffer.buffer),
+  ]);
+
+  return [
+    {
+      name: 'JetBrains Mono',
+      data: jetbrainsMono,
+      style: 'normal' as const,
+      weight: 400 as const,
+    },
+    {
+      name: 'Work Sans',
+      data: workSans,
+      style: 'normal' as const,
+      weight: 400 as const,
+    },
+  ]
+}
+
 export async function GET(_req: Request, { params }: RouteContext<'/og/docs/[...slug]'>) {
   const { slug } = await params;
+
   const page = source.getPage(slug.slice(0, -1));
   if (!page) notFound();
 
   return new ImageResponse(
-    <DefaultImage 
-      title={page.data.title} 
-      description={page.data.description || 'The Agentic Operating System Protocol'} 
-      site="OS Protocol" 
+    <OGImage
+      title={page.data.title}
+      description={page.data.description || 'The Agentic Operating System Protocol'}
+      site="os://protocol"
+      icon={page.data.icon}
+      primaryColor="#0c0a09"
+      primaryTextColor="#1570ef"
     />,
     {
       width: 1200,
       height: 630,
+      fonts: await loadFonts(),
     },
   );
 }
